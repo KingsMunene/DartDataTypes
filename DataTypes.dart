e30bc1955936394c6fb6e23f65a298af970a16fb
KingsMunene <munenekings2@gmail.com>
2024-02-17 18:44:06 +0300

    Added List, Map and Runes

--- DataTypes.dart
+++ DataTypes.dart
@@ -1,19 +1,36 @@
 void main(List<String> args) {
-  // Strings Data type
+  // Strings Data type. The variable userName stores a name which is a string
   String userName = "Kings Munene";
   print("Hello, $userName");
 
-  // Integer
+  // Integer. The variable age stores a number of type int
   int age = 22;
-  print("In ten years your age will be ${age + 10}");
+  print("In ten years your age will be ${age + 10} years old!");
 
-  // double
+  // double. The variable loanAmount and loanPayable store numbers with decimal points
   double loanAmount = 20000.00;
   double loanPayable = loanAmount + loanAmount * 0.13;
-  print("Your loan after a year is: $loanPayable");
+  print("Your loan after a year is: Ksh $loanPayable");
 
-  //Boolean
+  //Boolean. The variable accessGranted stores a boolean based on the expression
   bool accessGranted = age >= 18;
   print("Allowed to access the premises? $accessGranted");
+
+  //List. The names variable stores a list of Strings
+  List names = ["Kings", "Mercy", "Denis", "Wekesa"];
+  names.forEach((name) {print(name);});
+
+  // Maps. The map has keys of type int and value of type String
+  Map<int, String> fruitsData= {
+    1:"Oranges",
+    2:"Apples",
+    3:"Mangoes"
+  };
+  print("The fruit with the key 2 is ${fruitsData[2]};");
+
+  //Runes
+  var symbol ='\u2665'; 
+  print("Made with $symbol by Kings");
+
 
 }
